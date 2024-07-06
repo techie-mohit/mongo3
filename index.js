a985cbce7e3fdbf0c19abb426b620fd3f49d1fd1
techie-mohit <mohitkeshari2000@gmail.com>
2024-07-06 18:28:28 +0530

    mongo

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express= require("express");
 const app= express();
 const path= require("path");
-const mongoose= require("mongoose")
+const mongoose= require("mongoose");
+const Chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("views engine", "ejs");
@@ -17,6 +18,19 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
 
+
+const chat1= new Chat({
+  from: "abhishek",
+  to: "saquib",
+  msg: "what are you doing",
+  created_at: new Date()
+});
+
+chat1.save()
+.then((res)=>{
+  console.log(res);
+})
+.catch((err)=> console.log(err));
 
 
 
